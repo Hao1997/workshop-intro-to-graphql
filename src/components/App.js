8f478b2b12c20100cf22ab8f9f6694b6e1204466
Joey Tepperman <jmichaelt98@gmail.com>
2019-01-30 21:12:13 -0500

    Remove unused variables

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import { gql } from "apollo-boost";
 import { Query } from "react-apollo";
-import logo from "../logo.svg";
 import "../styles/App.css";
-import InputName from "./InputName";
 
 class App extends Component {
   render() {
