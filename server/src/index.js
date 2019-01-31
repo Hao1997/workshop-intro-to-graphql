8181e43eecb84f43e58e3afcb1725af1d5dd2c23
Joey Tepperman <jmichaelt98@gmail.com>
2019-01-30 21:09:23 -0500

    Update server and graphql schema

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,58 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require("graphql-yoga");
+
+const db = {
+  blogs: [
+    {
+      id: 1,
+      name: "Joey's Blog"
+    },
+    {
+      id: 2,
+      name: "Ross's Blog"
+    }
+  ],
+  posts: [
+    {
+      id: 1,
+      title: "Joey's First Post",
+      text: "Hello"
+    },
+    {
+      id: 2,
+      title: "Joey's Second Post",
+      text: "World"
+    },
+    {
+      id: 3,
+      title: "Ross's First Post",
+      text: "Hello"
+    },
+    {
+      id: 4,
+      title: "Ross's Second Post",
+      text: "World"
+    }
+  ]
+};
 
 const typeDefs = `
-  type Query {
-    hello(name: String): String
+  type Blog {
+    id: ID!
+    name: String!
   }
-`
+
+  type Query {
+    blog(id: ID!): Blog
+    blogs: [Blog!]!
+  }
+`;
 
 const resolvers = {
   Query: {
-    hello: (_, args) => `Hello ${args.name || 'World'}!`,
-  },
-}
+    blog: (parent, args) => db.blogs.find(id => args.id),
+    blogs: (parent, args) => db.blogs
+  }
+};
 
-const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log(`Server is running at http://localhost:4000`))
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log(`Server is running at http://localhost:4000`));
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import { gql } from 'apollo-boost'
-import { Query } from 'react-apollo'
-import logo from '../logo.svg'
-import '../styles/App.css'
-import InputName from './InputName'
+import React, { Component } from "react";
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
+import logo from "../logo.svg";
+import "../styles/App.css";
+import InputName from "./InputName";
 
 class App extends Component {
   render() {
@@ -11,21 +11,19 @@ class App extends Component {
       <div className="App">
         <Query query={HELLO_QUERY}>
           {props => {
-            console.log(props)
-            const { data, loading, error, refetch } = props
+            console.log(props);
+            const { data, loading, error, refetch } = props;
             if (loading) {
-              return <div>Loading</div>
+              return <div>Loading</div>;
             }
 
             if (error) {
-              return <div>An unexpected error occurred</div>
+              return <div>An unexpected error occurred</div>;
             }
 
             return (
               <div>
-                <h1>
-                  Site Name
-                </h1>
+                <h1>Site Name</h1>
                 <div>
                   <select>
                     <option>Blog 1</option>
@@ -33,33 +31,28 @@ class App extends Component {
                     <option>Blog 3</option>
                   </select>
                 </div>
-                {
-                  [1, 2, 3].map((n) => (
-                    <article className="blogEntry">
-                      <h2>Article {n}</h2>
-                      <div>
-                        Author {n}
-                      </div>
-                      <p>
-                        Blog content
-                      </p>
-                    </article>
-                  )
-                  )
-                }
+                {[1, 2, 3].map(n => (
+                  <article className="blogEntry">
+                    <h2>Article {n}</h2>
+                    <div>Author {n}</div>
+                    <p>Blog content</p>
+                  </article>
+                ))}
               </div>
-            )
+            );
           }}
         </Query>
       </div>
-    )
+    );
   }
 }
 
 const HELLO_QUERY = gql`
-  query HelloQuery($name: String) {
-    hello(name: $name)
+  query BlogsQuery {
+    blogs {
+      name
+    }
   }
-`
+`;
 
-export default App
+export default App;
